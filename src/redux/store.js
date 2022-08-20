48fbdf34b1c2b69b59ce2e49cce2e52deb08ba95
Sharif Ahmed <sharifahmedewu15@gmail.com>
2022-08-21 00:40:52 +0600

    add middleware in react-redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,15 @@
-import { createStore } from "redux"
+import { applyMiddleware, createStore } from "redux"
 import rootReducer from "./rootReducer";
 
+const myLogger = (store) => (next) => (action) => {
+    console.log("Action100", JSON.stringify(action))
+    console.log("Before state", JSON.stringify(store.getState()))
 
-const store = createStore(rootReducer)
+    const upComingState = [action].reduce(rootReducer, store.getState())
+    console.log("Upcoming", JSON.stringify(upComingState))
+
+    return next(action)
+}
+const store = createStore(rootReducer, applyMiddleware(myLogger))
 
 export default store
